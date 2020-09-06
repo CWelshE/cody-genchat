b09ff752d9d6c7bd2624e978da51b8316bb7238c
Cody Welsh <codyw@protonmail.com>
2020-09-06 14:07:25 -0700

    feat(Styles): Set up grid
    Turns the App into a dynamic grid with smaller sidepanels; the resulting
    midpoint is larger than all other panels. (Also expands the height/width
    of parent elements to 100%.)

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -1,29 +1,51 @@
 // Primary entrypoint for the application.
-import "phoenix_html";
 import "../css/global.css";
+import "phoenix_html";
 
 // React deps
 import ReactDOM from "react-dom";
+import React from "react";
 
 // Styling deps
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
+import colorScheme from "./constants";
 
 const rootBase = 0;
-const gridTemplates = "repeat(3, 1fr)";
-const rootApp = (
-  <div
-    css={css`
-      margin: ${rootBase};
-      padding: ${rootBase};
-      display: grid;
-      grid-template-columns: ${gridTemplates};
-      grid-template-rows: ${gridTemplates};
-      font-size: 14px;
-      background-color: #ffffff;
-      border: 10px solid red;
-    `}
-  />
-);
+const gridTemplates = "1fr 3fr 1fr";
 
-ReactDOM.render(rootApp, document.getElementsByClassName("root")[0]);
+// A 3x3 grid that wraps around all the children elements
+// (The middle is 3x of the outside rows/columns)
+const RootWrapper = () => {
+  return (
+    <div
+      css={css`
+        display: grid;
+        grid-template-columns: ${gridTemplates};
+        grid-template-rows: ${gridTemplates};
+        height: 100%;
+        width: 100%;
+        margin: ${rootBase};
+        padding: ${rootBase};
+        font-size: 14px;
+        background-color: ${colorScheme.primary};
+      `}
+    >
+    </div>
+  );
+};
+
+class AppRoot extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <RootWrapper>
+        {this.props.children}
+      </RootWrapper>
+    );
+  }
+}
+
+ReactDOM.render(<AppRoot/>, document.getElementsByTagName("main")[0]);
