60752c373931d670b29c47f51a925f65a1e69d91
Cody Welsh <codyw@protonmail.com>
2020-09-06 22:21:53 -0700

    feat(aes.ex): Define encrypt/1
    Fills out the rest of the definition for our `encrypt/1` function within
    the AES module.

--- lib/encryption/aes.ex
+++ lib/encryption/aes.ex
@@ -1,11 +1,34 @@
 # Encryption methods for the app will be defined here.
 # Author: Cody Welsh <codyw at protonmail dot com>
 # (For Genity)
+#
+# This module definition is essentially an amalgamation of various
+# resources on encryption within Elixir/Phoenix.
 
 defmodule Encryption.AES do
   @aad "AES256GCM"
 
   # Base encryption function for text
   def encrypt(text) do
+    # Random initialization vector
+    init_vec = :crypto.strong_rand_bytes(16)
+
+    # TODO: Implement get_key/0 module def
+    encr_key = get_key()
+
+    # Encrypt the text; return cipher, cipher tag
+    {cipher, tag} =
+      :crypto.block_encrypt(
+        :aes_gcm,
+        encr_key,
+        init_vec,
+        {
+          @aad,
+          to_string(text),
+          16
+        }
+      )
+
+    iv <> tag <> cipher
   end
 end
