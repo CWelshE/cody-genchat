56d9c793c59c3d275f307105ca4ec88b14518e24
Cody Welsh <codyw@protonmail.com>
2020-09-06 21:51:58 -0700

    feat(Encryption): Initial AES module defs
    Add an encryption-specific module for usage elsewhere, when we are
    encrypting and verifying the following:
    
    - User names and passwords;
    - User email addresses;
    - Encryption keys;
    - P2P Messages.

---
+++ lib/encryption/aes.ex
@@ -0,0 +1,11 @@
+# Encryption methods for the app will be defined here.
+# Author: Cody Welsh <codyw at protonmail dot com>
+# (For Genity)
+
+defmodule Encryption.AES do
+  @aad "AES256GCM"
+
+  # Base encryption function for text
+  def encrypt(text) do
+  end
+end
