85b32ce7f635e98218ba3ef76071dd94ea94e4d9
Cody Welsh <codyw@protonmail.com>
2020-09-07 14:28:46 -0700

    feat(aes.ex): Define get_key/0
    Add fn definitions for:
    - get_key/0
    - get_key/1
    - get_key_id/0
    - encr_keys/0
    
    These functions are intended for basic key rotation with `n | n > 1` keys.
    
    These will likely need to be further defined in later commits.

--- lib/encryption/aes.ex
+++ lib/encryption/aes.ex
@@ -9,12 +9,13 @@ defmodule Encryption.AES do
   @aad "AES256GCM"
 
   # Base encryption function for text
+  # (encrypt/1)
   def encrypt(text) do
     # Random initialization vector
     init_vec = :crypto.strong_rand_bytes(16)
 
     # TODO: Implement get_key/0 module def
-    encr_key = get_key()
+    # Gets the most recent key in a rotation of keys
 
     # Encrypt the text; return cipher, cipher tag
     {cipher, tag} =
@@ -30,5 +31,32 @@ defmodule Encryption.AES do
       )
 
     iv <> tag <> cipher
+  end
+
+  # get_key/0
+  defp get_key do
+    get_key_id()
+    |> get_key
+  end
+
+  # get_key/1
+  # Fetch a key based on its id
+  defp get_key(id) do
+    encr_keys()
+    |> Enum.at(id)
+  end
+
+  # get_key/1
+  # Get an index corresponding to the latest key in our n-key rotation
+  defp get_key_id do
+    Enum.count(encr_keys()) - 1
+  end
+
+  # encr_keys/0
+  # "Application" is an Erlang library definition, and in this case will
+  # allow us to retrieve an encryption key from the app environment
+  defp encr_keys do
+    Application.get_env
+    (:encryption, Encryption.AES)[:keys]
   end
 end
