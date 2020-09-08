34d295512e051e15fb4e95f8cb10e7e9d71b3a7e
Cody Welsh <codyw@protonmail.com>
2020-09-07 19:08:27 -0700

    test(aes.ex): Add encrypt/1, decrypt/1 tests
    Add additional test cases for encrypt/1 and decrypt/1 to verify their
    functionality is as expected.

--- test/lib/encryption/aes_test.exs
+++ test/lib/encryption/aes_test.exs
@@ -2,11 +2,33 @@ defmodule Encryption.AESTest do
   use ExUnit.Case
   alias Encryption.AES
 
+  @same "this is the same string"
+  @same_cryp @same |> AES.encrypt()
+  @keys Application.get_env(:genchat, Encryption.AES)[:keys]
+
   test "encrypt/1 includes initialization vector in return values" do
     <<init_vec::binary-16, cipher::binary>> = AES.encrypt("test string")
 
     assert String.length(init_vec) != 0
     assert String.length(cipher) != 0
     assert is_binary(cipher)
+  end
+
+  test "encrypt/1 produces unique output with each invocation" do
+    assert AES.encrypt(@same) != AES.encrypt(@same)
+  end
+
+  test "decrypt/1 can decipher encrypt/1 output" do
+    assert @same |> AES.encrypt() |> AES.decrypt() == @same
+  end
+
+  test "decrypt/1 can decipher encrypt/1 after appending new keys" do
+    # Add a random key generated from Erlang
+    Application.put_env(:genchat, Encryption.AES, keys: @keys ++ [:crypto.strong_rand_bytes(32)])
+
+    assert @same == @same_cryp |> AES.decrypt()
+
+    # Restore the pre-test state
+    Application.put_env(:genchat, Encryption.AES, keys: @keys)
   end
 end
