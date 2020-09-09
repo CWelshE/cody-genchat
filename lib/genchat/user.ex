d47f9ecda82ebc943464d2b6b95b506b2597884f
Cody Welsh <codyw@protonmail.com>
2020-09-08 22:13:17 -0700

    feat(DB): Add hashed email attr on response
    Once we receive a changeset, push a hashed email attribute to the
    database along with it.

--- lib/genchat/user.ex
+++ lib/genchat/user.ex
@@ -17,5 +17,15 @@ defmodule Genchat.User do
     user
     |> cast(attrs, [:name, :email])
     |> validate_required([:email])
+    |> create_email_hash
+    |> unique_constraint(:email_hash)
+  end
+
+  defp create_email_hash(changeset) do
+    if Map.has_key?(changeset.changes, :email) do
+      changeset |> put_change(:email_hash, changeset.changes.email)
+    else
+      changeset
+    end
   end
 end
