9b2083bedcaaf9f68f8d3775ebc16d861b4f7c17
Cody Welsh <codyw@protonmail.com>
2020-09-10 21:32:12 -0700

    feat(Deps): Add Guardian for token auth
    Add Guardian as a dependency for authenticating JWT tokens in the
    application itself (for users).

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Genchat.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:argon2_elixir, ">= 1.3.0"}
+      {:argon2_elixir, ">= 1.3.0"},
+      {:guardian, ">= 0.13.0"}
     ]
   end
 
